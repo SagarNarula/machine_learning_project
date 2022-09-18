7c224697f61d6a381aed8e4ff2c7bc27d27c8800
Sagar Narula <41863364+SagarNarula@users.noreply.github.com>
2022-09-18 23:31:31 +0530

    folder structure updated

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing=HousingException(e,sys)
+        logging.info(housing.error_message)
+        #logging.info(housing)
+        logging.info("Testing the Logging File")
     return "Project has been started"
 
 
